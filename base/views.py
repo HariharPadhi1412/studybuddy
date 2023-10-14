7fafd86b137c5183c134cd2aa1d0667e1531e867
haripadhi08 <haripadhi08@gmail.com>
2023-10-14 11:16:08 +0530

    created the main and navbar template

--- base/views.py
+++ base/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("home")
+    return render(request,'home.html')
 
 def room(request):
-    return HttpResponse("rooms")
+    return render(request,'room.html')
