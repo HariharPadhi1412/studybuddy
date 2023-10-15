6068e5b429ed1d50e8414eb85a5941b483f8b4d9
haripadhi08 <haripadhi08@gmail.com>
2023-10-15 22:57:27 +0530

    created the topic and various other models & also created the superuser

--- base/views.py
+++ base/views.py
@@ -1,24 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Room
 # Create your views here.
 
-rooms = [
-    {'id':1,"name": "new room"},
-    {'id':2,"name": "new room 1"},
-    {'id':3,"name": "new room 2"},
-    {'id':4,"name": "new room 3"},
-]
-
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request,'base/home.html',context)
 
 def room(request,pk):
-    room = None
-
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room' : room}
     return render(request,'base/room.html',context)
