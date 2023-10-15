a6124a51ff27e42041d9d0115f43a6bd16071566
haripadhi08 <haripadhi08@gmail.com>
2023-10-15 19:48:23 +0530

    created the dynamic routing in django

--- base/views.py
+++ base/views.py
@@ -14,7 +14,11 @@ def home(request):
     context = {'rooms': rooms}
     return render(request,'base/home.html',context)
 
-def room(request):
-    context = {'rooms': rooms}
+def room(request,pk):
+    room = None
 
-    return render(request,'base/room.html')
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room' : room}
+    return render(request,'base/room.html',context)
